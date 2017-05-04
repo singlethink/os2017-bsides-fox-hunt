e3c1be17d7838c10e6f1a19b630f25ff51178132
Matthew Van Gundy <matt@singlethink.net>
2017-05-04 18:25:29 -0400

    Actually define values moved into config header

--- Arduino/libraries/OS2017_Common/OS2017_Common.h
+++ Arduino/libraries/OS2017_Common/OS2017_Common.h
@@ -1,7 +1,11 @@
 #ifndef OS2017_COMMON_H
 #define OS2017_COMMON_H
 
-#define OS2017_FOX_BIT_RATE  500   // bits per second
+#define OS2017_FOX_BIT_RATE             500  // bits per second
+#define OS2017_FOX_PKTS_PER_ROUND        30  // 30 pkts
+#define OS2017_FOX_INTER_PKT_DELAY_MS  1000  // 1s
+#define OS2017_FOX_INTER_MSG_DELAY     (30 * 1000) // 30 s
+
 
 #define OS2017_FOX0_ID         0
 #define OS2017_FOX1_ID         1
